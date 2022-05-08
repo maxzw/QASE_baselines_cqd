0cc61dfbc8d5b7c25b7f641f9fb175bed0031c00
Max <maxzwager+github@gmail.com>
2022-05-08 17:23:27 +0200

    fix

--- my_job.sh
+++ my_job.sh
@@ -27,4 +27,4 @@ source activate thesis
 srun python main.py --cuda --do_train --do_valid --do_test \
   --data_path data/AIFB-gqs -n 128 -b 512 -d 800 -g 24 \
   -lr 0.0001 --max_steps 450001 --cpu_num 1 --geo vec --valid_steps 15000 \
-  --tasks "1p.2p.3p.2i.3i.ip.pi" --checkpoint-path "logs/AIFB-gqs/1p.2p.3p.2i.3i.ip.pi/vec/g-24.0/2022.05.04-22:36:23/"
+  --tasks "1p.2p.3p.2i.3i.ip.pi" --checkpoint_path "logs/AIFB-gqs/1p.2p.3p.2i.3i.ip.pi/vec/g-24.0/2022.05.04-22:36:23/"
