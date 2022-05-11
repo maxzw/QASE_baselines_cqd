2ab9e0afc78eba98af4dd0a943608ed3d007d11b
maxzw <maxzwager+github@gmail.com>
2022-05-11 09:44:22 +0200

    added cqd script

---
+++ cqd_job.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_shared
+#SBATCH --time=100:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=m.j.zwager@student.vu.nl
+#SBATCH --output=job_logs/output_%A.out
+#SBATCH --error=job_logs/errors_%A.err
+
+module purge all
+module load 2021
+module load Anaconda3/2021.05
+
+# define and create a unique scratch directory
+SCRATCH_DIRECTORY=/global/work/${USER}/kelp/${SLURM_JOBID}
+mkdir -p ${SCRATCH_DIRECTORY}
+cd ${SCRATCH_DIRECTORY}
+
+# Activate Anaconda work environment for OpenDrift
+source /home/${USER}/.bashrc
+source activate thesis
+
+# Run your code
+srun python main.py --do_train --do_valid --do_test \
+    -n 1 -b 2000 -d 1000 -lr 0.1 --warm_up_steps 100000000 --max_steps 100000 \
+    --cpu_num 0 --geo cqd --valid_steps 500 --print_on_screen --save_checkpoint_steps 5000 \
+    --test_batch_size 1000 --optimizer adagrad --reg_weight 0.1 --log_steps 500 \
+    --cuda --use-qa-iterator ${@:1}
+
+# --data_path data/FB15k-237-q2b
+# --tasks 1p
