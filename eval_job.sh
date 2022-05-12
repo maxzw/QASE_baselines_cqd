2d2a92aefd11b96b98c1b9936cc8603153cc3b91
Max Zwager <Maxzwager+github@gmail.com>
2022-05-12 12:01:30 +0200

    big fix

---
+++ eval_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_shared
+#SBATCH --time=100:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=m.j.zwager@student.vu.nl
+#SBATCH --output=job_logs/output_%A.out
+#SBATCH --error=job_logs/errors_%A.err
+
+module purge all
+module load 2021
+module load Anaconda3/2021.05
+
+# define and create a unique scratch directory
+SCRATCH_DIRECTORY=/global/work/${USER}/kelp/${SLURM_JOBID}
+mkdir -p ${SCRATCH_DIRECTORY}
+cd ${SCRATCH_DIRECTORY}
+
+# Activate Anaconda work environment for OpenDrift
+source /home/${USER}/.bashrc
+source activate thesis
+
+# Run your code
+srun python main.py --cuda --do_valid --do_test \
+  -n 128 -b 512 -lr 0.0001 --max_steps 450001 --cpu_num 1 \
+  --valid_steps 15000 --save_checkpoint_steps 5000 \
+  --tasks "1p.2p.3p.2i.3i.ip.pi" ${@:1}
